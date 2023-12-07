cdc5660561a51ebcb7d80717c4df03be0013a07d
Guillermo Ojeda <gro1413@hotmail.com>
2023-12-07 19:02:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, Container } from "@mui/material";
+//import Products from "./components/Products";
+
+export default function App() {
+
+  return (
+    <Container maxWidth="xl">
+      {/*<Products/>*/}
+      <Button variant="contained">Hola mundo</Button>
+    </Container>
+  )
+}
